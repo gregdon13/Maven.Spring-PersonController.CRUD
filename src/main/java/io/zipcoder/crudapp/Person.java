0d07f7991f768e81b360ed408eeaa598252bfe1a
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-04-15 16:48:42 -0400

    checked it in postman. All methods are working

---
+++ src/main/java/io/zipcoder/crudapp/Person.java
@@ -0,0 +1,40 @@
+package io.zipcoder.crudapp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    String firstName;
+    String lastName;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Id
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/main/java/io/zipcoder/crudapp/PersonController.java
@@ -0,0 +1,50 @@
+package io.zipcoder.crudapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class PersonController {
+
+    @Autowired
+    private final PersonRepository personRepository;
+
+    PersonController(PersonRepository repository) {
+        this.personRepository = repository;
+    }
+
+    @PostMapping("/people")
+    public Person createPerson(Person p) {
+        return personRepository.save(p);
+    }
+
+    @GetMapping("/people/{id}")
+    public Person getPerson(@PathVariable int id) {
+        return personRepository.findOne(id);
+    }
+
+    @GetMapping("/people")
+    public List<Person> getPersonList() {
+        List<Person> outList = new ArrayList<>();
+        for (Person p : personRepository.findAll()) {
+            outList.add(p);
+        }
+        return outList;
+    }
+
+    @PutMapping("/people/{id}")
+    public Person updatePerson(Person p) {
+        Person holder = personRepository.findOne(p.getId());
+        holder.setFirstName(p.firstName);
+        holder.setLastName(p.lastName);
+        return personRepository.save(holder);
+    }
+
+    @DeleteMapping("/people/{id}")
+    public void deletePerson(@PathVariable int id) {
+        personRepository.delete(id);
+    }
+}
